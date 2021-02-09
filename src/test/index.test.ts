adab933540373c3e3add65374160deaa2e25e5bf
Ashley Davis <ashley@codecapers.com.au>
2021-02-10 08:33:36 +1000

    Diverged from the template. Now ready to start adding real code.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,10 +1,10 @@
-import { ExampleClass } from "../index";
+
 
 describe("main test suite", () => {
 
     it("should be true", ()  => {
 
-        expect((new ExampleClass()).returnsTrue()).toBeTruthy();
+        expect(true).toBeTruthy();
 
     });
 
